70e97ac3efae946f60ea608cced56776752946bc
max-student <max-student@maxtrain.com>
2020-02-19 15:50:31 -0500

    Add project files.

---
+++ SQLlibrary/BcConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLlibrary {
+    public class BcConnection {
+
+        public SqlConnection Connection { get; set; } 
+
+        public void Connect(string server, string database, string auth) {
+            var connStr = $"server = {server}; database = {database}; {auth}";
+            Connection = new SqlConnection(connStr);
+            Connection.Open(); //this is an example of one where you might want to throw a try catch lock since it can have 3 exceptions
+            if(Connection.State != System.Data.ConnectionState.Open) {
+                Console.WriteLine("Could not open the connection. Check connection string.");
+                Connection = null;
+                return;
+            }
+            Console.WriteLine("Connection opened");
+        }
+
+    }
+}
